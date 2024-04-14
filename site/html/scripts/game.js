0531fc7789d0e919e2b10f7784321a635e194e3e
harshitakeer <115908428+harshitakeer@users.noreply.github.com>
2024-04-14 16:17:26 -0700

    Update game.js

--- site/html/scripts/game.js
+++ site/html/scripts/game.js
@@ -1,4 +1,4 @@
-let timeLeft = 60;
+let timeLeft = 45;
 let timerInterval;  
 let currentWindmillStage = 1;
 let currentTownStage = 0;
@@ -58,25 +58,26 @@ function endGame(win) {
 }
 
 function restartGame() {
-    
+
     clearInterval(timerInterval);
-   
-    timeLeft = 60;
+
+    timeLeft = 45;
     currentWindmillStage = 1;
     currentTownStage = 0;
     clickCount = 0;
 
     document.getElementById('windmill').style.backgroundImage = "url('../photos/windmill1.png')";
-    document.getElementById('town').className = 'unlit';
+    document.getElementById('town').style.backgroundImage = "url('../photos/unlittown.png')";
     document.getElementById('town').style.display = 'block';
     document.getElementById('windmill').style.display = 'block'; 
 
     document.getElementById('time-display').textContent = timeLeft;
 
-    document.getElementById('message-box').style.display = 'none';
-    document.getElementById('restart-button').style.display = 'none';
+    document.getElementById('game-over').style.display = 'none';
+    document.getElementById('win-message').style.display = 'none';
+    document.getElementById('lose-message').style.display = 'none';
 
-    document.getElementById('game-area').style.display = 'block';
+    document.getElementById('game').style.display = 'block';
 
     startTimer();
 }
