0bb181f422e2662336f5d23596c20415b3eeda51
harshitakeer <115908428+harshitakeer@users.noreply.github.com>
2024-04-14 12:05:31 -0700

    Update about.js

--- site/html/scripts/about.js
+++ site/html/scripts/about.js
@@ -1,13 +1,14 @@
-document.getElementById('meet-team-btn').addEventListener('click', function() {
-    var teamSection = document.getElementById('team');
-    // Toggle the 'show' class
-    if (teamSection.classList.contains('show')) {
-        teamSection.classList.remove('show');
-        setTimeout(() => { teamSection.style.display = 'none'; }, 500); // Wait for the transition to finish.
-    } else {
-        teamSection.style.display = 'block';
-        setTimeout(() => { teamSection.classList.add('show'); }, 10); // Slight delay to ensure display change is applied.
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleMissionBtn = document.getElementById('meet-team-btn');
+    var missionSection = document.getElementById('mission');
+
+    toggleMissionBtn.addEventListener('click', function() {
+        var isMissionHidden = missionSection.style.display === 'none';
+        
+        missionSection.style.display = isMissionHidden ? 'block' : 'none';
+        toggleMissionBtn.textContent = isMissionHidden ? 'Show Less' : 'Learn More';
+    });
 });
+
 
 
