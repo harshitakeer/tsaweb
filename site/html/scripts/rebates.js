a155c14ff4d3ce18c8068e1207874e217587f1ab
harshitakeer <115908428+harshitakeer@users.noreply.github.com>
2024-04-14 12:04:30 -0700

    Update rebates.js

--- site/html/scripts/rebates.js
+++ site/html/scripts/rebates.js
@@ -1,18 +1,14 @@
 function updateStateIncentives() {
-    console.log("Function is called."); // Check if function is triggered
     var stateSelector = document.getElementById('state-selector');
     var allInfos = document.querySelectorAll('.state-info');
-    allInfos.forEach(div => div.style.display = 'none'); // Hide all info divs
+    allInfos.forEach(div => div.style.display = 'none');
 
     var selectedValue = stateSelector.value;
-    console.log("Selected Value:", selectedValue); // Debug selected value
-
-    var selectedInfoDiv = document.getElementById('info' + selectedValue);
+    var selectedInfoDiv = document.getElementById('state-specific-info-' + selectedValue);
     if (selectedInfoDiv) {
-        selectedInfoDiv.style.display = 'block'; // Show the selected state's info
+        selectedInfoDiv.style.display = 'block';
     } else {
         console.log("No div found for selected state.");
     }
 }
-
 
